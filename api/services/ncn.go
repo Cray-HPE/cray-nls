391fae4d83ac442c74fce5f03a23ce846cad1f79
Di Zou <di.zou@hpe.com>
2022-04-03 14:52:25 -0400

    clean up code from PoC

--- api/services/ncn.go
+++ api/services/ncn.go
@@ -25,7 +25,6 @@ package services
 
 import (
 	"github.com/Cray-HPE/cray-nls/utils"
-	"gorm.io/gorm"
 )
 
 // NcnService service layer
@@ -38,9 +37,4 @@ func NewNcnService(logger utils.Logger) NcnService {
 	return NcnService{
 		logger: logger,
 	}
-}
-
-// WithTrx delegates transaction to repository database
-func (s NcnService) WithTrx(trxHandle *gorm.DB) NcnService {
-	return s
 }
