3a1f5ee31d38362c92425d765d4c8a3f0d14f525
Di Zou <di.zou@hpe.com>
2022-04-22 14:22:34 -0400

    solve locking issue

--- scripts/argo.local.sh
+++ scripts/argo.local.sh
@@ -31,12 +31,13 @@ k3d cluster list  | grep "mycluster"
 
 if [[ $? -ne 0 ]]; then
     k3d cluster create mycluster \
-        -a 2 \
+        -a 3 \
         --agents-memory 1g \
         --servers-memory 1g \
         --no-lb \
         --k3s-arg "--node-name=ncn-w001"@agent:0 \
         --k3s-arg "--node-name=ncn-w002"@agent:1 \
+        --k3s-arg "--node-name=ncn-w003"@agent:2 \
         --k3s-arg "--node-name=ncn-m001"@server:0
 else 
     k3d cluster start --wait
@@ -53,3 +54,6 @@ kubectl create ns argo
 kubectl apply -n argo -f https://raw.githubusercontent.com/argoproj/argo-workflows/master/manifests/quick-start-postgres.yaml
 kubectl wait --for=condition=ready pod -l app=argo-server -n argo
 kubectl -n argo port-forward svc/argo-server 2746:2746
+
+
+#   TODO:  we need cluster role binding so argo can add/remove labels to a worker node
