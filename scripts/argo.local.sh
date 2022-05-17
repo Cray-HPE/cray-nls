6e8ecd87fb677b8ff0cd2423875fb058ad4feaed
Di Zou <di.zou@hpe.com>
2022-05-17 11:10:13 -0400

    update temp dev deployment

--- scripts/argo.local.sh
+++ scripts/argo.local.sh
@@ -51,7 +51,7 @@ kubectl taint nodes ncn-m001 node-role.kubernetes.io/master=:NoSchedule
 docker ps | awk '/mycluster/ {print $1}' | xargs -I '{}' docker exec '{}' sh -c "mkdir -p /root/.ssh"
 
 kubectl create ns argo
-kubectl apply -n argo -f https://raw.githubusercontent.com/argoproj/argo-workflows/master/manifests/quick-start-postgres.yaml
+kubectl apply -n argo -f scripts/quick-start-postgres.yaml
 kubectl wait --for=condition=ready pod -l app=argo-server -n argo
 kubectl -n argo port-forward svc/argo-server 2746:2746
 
