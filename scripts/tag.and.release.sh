7d6fb69140eff022c8418ed181b739a85377de8c
Di Zou <di.zou@hpe.com>
2022-11-18 09:43:36 -0500

    [ci] introduce tag and release script

--- scripts/tag.and.release.sh
+++ scripts/tag.and.release.sh
@@ -20,43 +20,7 @@
 # OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
 # ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 # OTHER DEALINGS IN THE SOFTWARE.
-
-IMAGE_NAME="cray-nls"
-
-usage() {
-        echo "$FUNCNAME: $0"
-        echo "  -h | prints this help message" 
-        echo "  -l | hostname to push to, default localhost";
-        echo "  -r | repo to push to, default cray";
-        echo "  -f | forces build with --no-cache and --pull";
-	echo "";
-        exit 0
-}
-
-
-REPO="cray"
-REGISTRY_HOSTNAME="localhost"
-FORCE=" "
-
-while getopts "hfl:r:" opt; do
-  case $opt in
-    h) 
-      usage; 
-      exit;;
-    f) 
-      FORCE=" --no-cache --pull";;
-    l) 
-      REGISTRY_HOSTNAME=${OPTARG};;
-    r) 
-      REPO=${OPTARG};;
-  esac
-done
-
-shift $((OPTIND-1))
-
-echo "Building $FORCE and pushing to $REGISTRY_HOSTNAME in repo $REPO"
-
-set -e
-docker build $FORCE -t cray/$IMAGE_NAME .
-docker tag cray/$IMAGE_NAME $REGISTRY_HOSTNAME/$REPO/$IMAGE_NAME
-docker push $REGISTRY_HOSTNAME/$REPO/$IMAGE_NAME
+git checkout master
+git pull
+git tag v$(cat .version)
+git push --tags
