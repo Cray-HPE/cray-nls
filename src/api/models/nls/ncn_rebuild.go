9417d54259f7cc423860b062631393f9e762a512
Atif Ali <atif.ali@hpe.com>
2023-07-19 13:39:51 -0400

    CASM-4467 release 1.5 (#193)
    * CASM-4467 iuf-container is using a buggy version of cray-cli
    
    * CASM-4467 cherry pick items from main branch as they were not pushed to release/1.5
    
    ---------
    
    Co-authored-by: Di Zou <di.zou@hpe.com>

--- src/api/models/nls/ncn_rebuild.go
+++ src/api/models/nls/ncn_rebuild.go
@@ -30,14 +30,15 @@ import (
 )
 
 type CreateRebuildWorkflowRequest struct {
-	Hosts            []string          `json:"hosts"`
-	DryRun           bool              `json:"dryRun"`
-	SwitchPassword   string            `json:"switchPassword,omitempty"`
-	ZapOsds          bool              `json:"zapOsds,omitempty"`      // this is necessary for storage rebuilds when unable to wipe the node prior to rebuild
-	WorkflowType     string            `json:"workflowType,omitempty"` // used to determine storage rebuild vs upgrade
-	ImageId          string            `json:"imageId,omitempty"`
-	DesiredCfsConfig string            `json:"desiredCfsConfig,omitempty"`
-	Labels           map[string]string `json:"labels,omitempty"`
+	Hosts                []string          `json:"hosts"`
+	DryRun               bool              `json:"dryRun"`
+	SwitchPassword       string            `json:"switchPassword,omitempty"`
+	ZapOsds              bool              `json:"zapOsds,omitempty"`      // this is necessary for storage rebuilds when unable to wipe the node prior to rebuild
+	WorkflowType         string            `json:"workflowType,omitempty"` // used to determine storage rebuild vs upgrade
+	ImageId              string            `json:"imageId,omitempty"`
+	DesiredCfsConfig     string            `json:"desiredCfsConfig,omitempty"`
+	Labels               map[string]string `json:"labels,omitempty"`
+	BootTimeoutInSeconds int               `json:"bootTimeoutInSeconds,omitempty"`
 }
 
 type CreateRebuildWorkflowResponse struct {
